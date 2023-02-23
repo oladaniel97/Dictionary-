590c1682f935e27bd716757827bc9f345fefb3ff
daniel amodu <danielolayinka97@gmail.com>
2023-02-23 23:53:06 +0100

    .

--- dictionary/dictionary/settings.py
+++ dictionary/dictionary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9v+_e(!&tahdhmq$!!k&db$iy^m^i2wzs(pgi3rn5!ee8@eu+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
