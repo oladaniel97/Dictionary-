d5eae640de5f4897877b227fd25125dc13baf6ba
daniel amodu <danielolayinka97@gmail.com>
2023-03-02 11:52:24 +0100

    .

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9v+_e(!&tahdhmq$!!k&db$iy^m^i2wzs(pgi3rn5!ee8@eu+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -74,24 +74,13 @@ WSGI_APPLICATION = 'dictionary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'diction',
-       'USER': 'postgres',
-       'PASSWORD': 'oladan',
-       'HOST': 'localhost',
-       'PORT': 5432,
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -128,9 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
